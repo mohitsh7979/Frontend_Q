19fadfb39614afd775233683af95e81eb7b4e5dd
mohitsh7979 <mohitsh7979@gmail.com>
2024-06-19 16:31:12 +0530

    Udate

---
+++ JavaScript/index1.js
@@ -0,0 +1,7 @@
+// console.log("Hello")
+// for(let i = 0;i<10;i++){
+//     console.log(i)
+// }
+
+// Write a java script program to print sum of digits 
+// Write a java script program to print all palindrom number between 1 to n.
---
+++ JavaScript/string.js
@@ -0,0 +1,54 @@
+// primitive --> number , string , boolean , undefined , null , symbol (call by value)
+// not primitive --> objects call by refrence
+
+// let my = "   Aohit Sharma  ";
+// console.log(name[0])
+    // console.log(typeof name)
+    // console.log(my.length)
+    // for(let i=0;i<my.length+10;i++){
+    //     console.log(my[i])
+    // }
+// console.log(my.toLowerCase())
+// console.log(my.toUpperCase())
+// console.log(my.slice(-9))
+// console.log(my.replace("mo","rx"))
+// console.log(my.charAt(7))
+// console.log(my.charCodeAt(3))
+
+// let x = "     mohit     ";
+
+// console.log(x.trim())
+
+// let x = "mohit";
+// let y = "sharma";
+
+// console.log(x.concat(y))
+
+// let x = "1-2-3-4-5,5,6,7,8,9";
+// let y = x.split("-");
+// console.log(y)
+// console.log(typeof y)
+
+// length
+// toLowerCase
+// toUpperCase 
+// slice 
+// charAt
+// charcodeAt
+// replace
+// trim 
+// concat 
+// split
+
+// let x = ["vivo v20 se","vivo v25 ","vivo zyx","samsung galaxy s21","oppo f12","apple iphone 15"];
+
+// let vivo = x.filter((item)=>{
+    
+//     return item.includes("vivo");
+// })
+
+// console.log(vivo)
+
+
+
+
