725645d1b2945d8cbd7b1748da0d3eff73e587e3
mohitsh7979 <mohitsh7979@gmail.com>
2024-06-20 16:39:09 +0530

    UPdate

---
+++ JavaScript/function.js
@@ -0,0 +1,57 @@
+// let a = 10;
+// let b = 20;
+// let c = 7;
+// let d = 2;
+// let e = 6;
+// let f = 3;
+
+// console.log(a+b)
+// console.log(c-d)
+// console.log(a*b)
+// console.log(e/f)
+
+// function add(a,c,...b){
+//     console.log(a)
+//     console.log(c)
+//     console.log(b)
+
+// }
+
+// add(5,7,8,9,5,89,978,66);
+// function minus(a,b){
+//     console.log(a-b)
+// }
+// function mul(a,b){
+//     console.log(a*b)
+// }
+// function divide(a,b){
+//     console.log(a/b)
+// }
+
+
+
+
+// minus(10,20)
+// minus(4,2)
+// mul(10,20)
+// divide(4,2)
+// divide(8,9)
+
+
+// let a = [1,2,3,4,5];
+// let b = [10,20,30,40,50];
+
+// let c = [...a,...b]
+
+// console.log(c)
+
+let [a,b,...c] = [10,20,30,40,50,60,70,80,90,100];
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+
+
+
+
