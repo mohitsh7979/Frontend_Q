056e0ec3daa1ff3a0653d8d65e4c1da83e028890
mohitsh7979 <mohitsh7979@gmail.com>
2024-07-16 18:08:59 +0530

    Uopdare

--- JavaScript/index1.js
+++
@@ -1,7 +0,0 @@
-// console.log("Hello")
-// for(let i = 0;i<10;i++){
-//     console.log(i)
-// }
-
-// Write a java script program to print sum of digits 
-// Write a java script program to print all palindrom number between 1 to n.
